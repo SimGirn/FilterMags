0dfde2413cf9cb83cf6c2981cb958d97fe41f7a8
Katherine <kpanebianco@me.com>
2026-06-25 11:27:05 -0700

    Add test folder

---
+++ Tests/filtermag_tests.py
@@ -0,0 +1,5 @@
+# list of tests to write
+# unit test for ``read_spectrum`` (with tutorial spectra)
+# unit test for ``filter_mag`` (with tutorial spectra, r and i filters)
+# unit test for when filter is out of range
+# end-to-end test for tutorial spectra using just ``read_spectrum`` and ``filter_mag``
